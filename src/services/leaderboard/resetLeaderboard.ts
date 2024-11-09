5fd7c29a395978d0c118e4c57e0f29344342ef25
Ozgur Coskuner <ozgur@decktopus.com>
2024-11-09 17:12:29 +0300

    adding comment for an unwise code block

--- src/services/leaderboard/resetLeaderboard.ts
+++ src/services/leaderboard/resetLeaderboard.ts
@@ -4,6 +4,10 @@ import { LEADERBOARD_WEEKLY } from "../../constants";
 export const resetLeaderboard = async () => {
   const allPlayers = await client.zRangeWithScores(LEADERBOARD_WEEKLY, 0, -1);
   const pipeline = client.multi();
+
+  // this part written with huge disgust
+  // in order to display leaderboard after weekly reset, player scores are assignin to 0 instead of dropping table
+
   allPlayers.forEach(({ value }) =>
     pipeline.zAdd(LEADERBOARD_WEEKLY, { value, score: 0 })
   );
